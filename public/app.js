2b4393408685a2fbc600b602bf4f03ee0aac16fc
nogaasha <sivosha2010@gmail.com>
2016-04-07 21:42:39 +0300

    update add_to card function

--- public/app.js
+++ public/app.js
@@ -1,9 +1,15 @@
 function add_to_cart(id)
 {
+	var key = 'product_' + id;
+
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
 
 function something()
 {
+	
 	var x = window.localStorage.getItem('aaa');
 	x = x * 1 + 1;
 	window.localStorage.setItem('aaa',x);
