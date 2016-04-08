87085d2ff8876e960f28eee79f46616b3c43f988
nogaasha <sivosha2010@gmail.com>
2016-04-08 21:12:58 +0300

    add cart_get_orders()

--- public/app.js
+++ public/app.js
@@ -45,3 +45,20 @@ function something()
 	alert(x);
 	x=x+1;
 }
+function cart_get_orders()
+{
+	var orders = '';
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			orders = orders + key + '=' + value + ',';
+		}
+	}
+
+	return orders;
+}
