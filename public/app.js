0d8de998d263f4e25271662000e868257b8974aa
nogaasha <sivosha2010@gmail.com>
2016-04-08 20:40:20 +0300

    add card element to nav

--- public/app.js
+++ public/app.js
@@ -18,6 +18,24 @@ function enumerate_l_s()
  	alert(x)
 }
 
+function cart_get_number_of_items()
+{
+	var cnt = 0;
+
+	for(var i = 0; i < window.localStorage.length; i++)
+	{
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
+		{
+			cnt = cnt + value * 1;
+		}
+	}
+
+	return cnt;
+}
+
 function something()
 {
 
