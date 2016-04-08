db141f055519efa830d469c2f8c38231bf0b2d89
nogaasha <sivosha2010@gmail.com>
2016-04-08 20:16:24 +0300

    add card_number_of_items

--- public/app.js
+++ public/app.js
@@ -7,9 +7,20 @@ function add_to_cart(id)
 	window.localStorage.setItem(key, x);
 }
 
+function enumerate_l_s()
+{
+	var x = 0
+	for(var i=0, len=localStorage.length; i<len; i++)  {
+		 var key = localStorage.key(i);
+		 var value = localStorage[key];
+		 x = x * 1 + value * 1;
+ 	}
+ 	alert(x)
+}
+
 function something()
 {
-	
+
 	var x = window.localStorage.getItem('aaa');
 	x = x * 1 + 1;
 	window.localStorage.setItem('aaa',x);
