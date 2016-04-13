3515b76ad3110bba9e9295782f6b3ae42bb644d8
nogaasha <sivosha2010@gmail.com>
2016-04-13 19:03:14 +0300

    parse string

--- public/app.js
+++ public/app.js
@@ -5,9 +5,26 @@ function add_to_cart(id)
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
+
+	update_orders_input();
+	update_orders_button();
 }
 
-function enumerate_l_s()
+function update_orders_input()
+{
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
+
+
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
+}
+
+
+function enumerate_l_s()   //я написала
 {
 	var x = 0
 	for(var i=0, len=localStorage.length; i<len; i++)  {
@@ -18,7 +35,7 @@ function enumerate_l_s()
  	alert(x)
 }
 
-function cart_get_number_of_items()
+function cart_get_number_of_items()  //считает кол. позиций в корзине
 {
 	var cnt = 0;
 
@@ -62,3 +79,15 @@ function cart_get_orders()
 
 	return orders;
 }
+function cancel_order()
+{
+	window.localStorage.clear();
+
+	update_orders_input();
+	update_orders_button();
+
+	$('#cart').text('Your cart is now empty');
+
+	return false;	
+}
+
